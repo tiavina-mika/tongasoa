4d1294fa1bf041231999d702fc79752f7e7bbd40
tiavina-mika <tiavinamika@gmail.com>
2025-10-08 08:12:46 +0300

    add image

--- src/pages/Tongasoa.tsx
+++ src/pages/Tongasoa.tsx
@@ -1,10 +1,13 @@
+
 import type { TongasoaProps } from '../types';
 import '../styles/index.css';
 import Title from '../components/Title';
+import reactLogo from '../assets/react.svg';
 
 const Tongasoa = ({ name = 'World' }: TongasoaProps) => {
   return (
     <div>
+      <img src={reactLogo} alt="Logo" width="60" />
       <Title />
       <h2>Welcome to {name}!</h2>
     </div>
