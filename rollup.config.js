39f29fd71c280bebf91ade7438c888464a618c31
tiavina-mika <tiavinamika@gmail.com>
2025-10-13 21:27:37 +0300

    feat: test version incrementation

--- rollup.config.js
+++ rollup.config.js
@@ -34,6 +34,7 @@ export default [
     plugins: [
       clear({ targets: [DIST_DIR], watch: true }),
       aliasConfig,
+      // Handle asset files
       url({
         include: [
           '**/*.svg',
