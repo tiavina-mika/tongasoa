23ba5ff83037700be5c714f620f6b9ed9d4fc1cd
tiavina-mika <tiavinamika@gmail.com>
2025-10-13 21:53:40 +0300

    BREAKING CHANGE: test major version upgrade

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -7,16 +7,16 @@ export default {
       'always',
       // Always apply this rule
       [
-        'feat', // New feature (major or minor changes, e.g., adding a new component)
-        'fix', // Bug fix (patch changes, e.g., fixing a bug in an existing component)
-        'docs', // Documentation changes (readme, comments, etc.)
-        'style', // Code style changes (formatting, missing semi colons, etc.)
-        'refactor', // Code refactoring (no feature or bug changes)
-        'perf', // Performance improvements
-        'test', // Adding or updating tests
-        'chore', // Maintenance tasks (build, tooling, etc.)
+        'feat', // New feature (major or minor changes, e.g., adding a new component): 1.0.0 => 1.1.0
+        'fix', // Bug fix (patch changes, e.g., fixing a bug in an existing component): 1.0.0 => 1.0.1
+        'docs', // Documentation changes (readme, comments, etc.): no version bump
+        'style', // Code style changes (formatting, missing semi colons, etc.): no version bump
+        'refactor', // Code refactoring (no feature or bug changes): no version bump
+        'perf', // Performance improvements : no version bump
+        'test', // Adding or updating tests : no version bump
+        'chore', // Maintenance tasks (build, tooling, etc.): no version bump
         'revert', // Reverting previous commits
-        'breaking', // BREAKING CHANGE (major changes that break backward compatibility)
+        'breaking', // BREAKING CHANGE (major changes that break backward compatibility): 1.0.0 => 2.0.0
       ],
     ],
     // Enforces that the commit subject uses sentence case
