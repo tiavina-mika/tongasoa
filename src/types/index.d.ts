b2531d4b3aee7714601f4896d27d1035ec1ab74d
tiavina-mika <tiavinamika@gmail.com>
2025-10-16 18:51:45 +0300

    fix: export lib

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,3 @@
 export * from '@/types/containers';
+export { default as Tongasoa } from './pages/Tongasoa';
+export { default } from './pages/Tongasoa';
---
+++ src/types/pages/Tongasoa.d.ts
@@ -0,0 +1,4 @@
+import type { FC } from 'react';
+import type { TongasoaProps } from '../containers';
+declare const Tongasoa: FC<TongasoaProps>;
+export default Tongasoa;
