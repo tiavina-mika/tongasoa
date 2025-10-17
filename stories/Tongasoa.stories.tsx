8e696abe6e83a1242369194d0b2757d3e56fb03e
tiavina-mika <tiavinamika@gmail.com>
2025-10-17 22:00:33 +0300

    fix: stories

--- stories/Tongasoa.stories.tsx
+++ stories/Tongasoa.stories.tsx
@@ -11,10 +11,28 @@ const meta: Meta<typeof Tongasoa> = {
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
+  argTypes: {
+    name: {
+      control: 'text',
+      description: 'The name to be displayed in the greeting message.',
+      defaultValue: 'World',
+    },
+    isTitleVisible: {
+      control: 'boolean',
+      description: 'Determines whether the title is visible.',
+      defaultValue: true,
+    },
+  },
 };
 
 export default meta;
 
 export const Default: StoryObj<typeof Tongasoa> = {
   args: {},
+};
+
+export const WithoutTitle: StoryObj<typeof Tongasoa> = {
+  args: {
+    isTitleVisible: false,
+  },
 };
