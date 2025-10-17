64aaf2e40ab9a849b25ab5603642709e4dcc5c37
tiavina-mika <tiavinamika@gmail.com>
2025-10-17 20:01:39 +0300

    Update App.tsx

--- demos/src/App.tsx
+++ demos/src/App.tsx
@@ -5,12 +5,10 @@ const name: TongasoaProps['name'] = 'Mik';
 
 const App = () => {
   return (
-    <>
-      <div>
-        <h1>Demo to test the package Tongasoa</h1>
-      </div>
-      <Tongosoa name={name} />
-    </>
+    <div className="demo-app">
+      <h1>Demo to test the package Tongasoa</h1>
+      <Tongosoa name={name} isTitleVisible={false} />
+    </div>
   );
 };
 
